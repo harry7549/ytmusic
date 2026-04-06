706782030af4f1bdef2ce10b132c0d3da4d36078
HardikS <hardiks@itoneclick.com>
2026-04-06 16:13:23 +0530

    mobile app

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
   images: {
     domains: ['i.ytimg.com', 'yt3.ggpht.com'],
-    unoptimized: true, // required for static export
   },
 };
 
