3049922b1eff105d90b933f89558349a6826cfe6
HardikS <hardiks@itoneclick.com>
2026-04-06 16:13:10 +0530

    mobile app

---
+++ android/app/src/main/java/com/yourname/ytmusic/MainActivity.java
@@ -0,0 +1,5 @@
+package com.yourname.ytmusic;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
     domains: ['i.ytimg.com', 'yt3.ggpht.com'],
+    unoptimized: true, // required for static export
   },
 };
 
